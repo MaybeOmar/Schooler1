8d4fe85f54637c695e0fbe8485b188dc3190ed35
MaybeOmar <yasseromar804@gmail.com>
2024-04-06 22:01:48 +0200

    updated

--- app/src/main/java/com/example/schoolmanagementsystem/AttendanceActivity.java
+++ app/src/main/java/com/example/schoolmanagementsystem/AttendanceActivity.java
@@ -1,50 +1,40 @@
 package com.example.schoolmanagementsystem;
+
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
-
-import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.example.schoolmanagementsystem.Models.Model;
 import com.example.schoolmanagementsystem.adapters.Adapter;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class teacher_home_page extends AppCompatActivity {
+public class AttendanceActivity extends AppCompatActivity {
+    Button profile_btn,settings_btn;
     private List<Model> modelList;
     RecyclerView RV;
     Adapter adapter;
     Intent intentprofile,intentsettings;
 
-    Button profile_btn,settings_btn;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_home_page);
+        setContentView(R.layout.activity_attendance_activity);
 
         modelList = new ArrayList<>();
-        modelList.add(new Model(R.drawable.subjectwork1, SubjectWorkActivity.class,"Class Work"));
-        modelList.add(new Model(R.drawable.generateexam, student_home_page.class,"Generate Exam"));
-        modelList.add(new Model(R.drawable.announcement, login_page.class,"Announcements"));
-        modelList.add(new Model(R.drawable.announcement, login_page.class,"Announcements"));
-
-
-
+        modelList.add(new Model(R.drawable.a_attend, student_home_page.class,"View Attendance"));
+        modelList.add(new Model(R.drawable.a_attend, SelectAttendInfoActivity.class,"Take Attendance"));
 
 
         //recyclerView
         RV = findViewById(R.id.id_recyclerview);
         RV.setLayoutManager(new GridLayoutManager(this,2));
-
 
         adapter = new Adapter(modelList);
         RV.setAdapter(adapter);
@@ -54,29 +44,19 @@ public class teacher_home_page extends AppCompatActivity {
         settings_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intentsettings = new Intent(teacher_home_page.this,settings.class);
+                intentsettings = new Intent(AttendanceActivity.this, settings.class);
                 startActivity(intentsettings);
             }
         });
 
-
-
-
         //profile button
-
         profile_btn=findViewById(R.id.profile_button);
-       profile_btn.setOnClickListener(new View.OnClickListener() {
+        profile_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intentprofile = new Intent(teacher_home_page.this,profile.class);
+                intentprofile = new Intent(AttendanceActivity.this, profile.class);
                 startActivity(intentprofile);
             }
         });
-
-
-
-
     }
-
-
 }
